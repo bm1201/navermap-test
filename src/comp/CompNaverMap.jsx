0d2a75e39b5d58d7a26b8e8ee9936e7a7cb69fd1
bm1201 <bm941201@gmail.com>
2024-04-24 09:56:34 +0900

    헤더는 줄바꿈 전까지 description은 줄바꿈 다음부터네

--- src/comp/CompNaverMap.jsx
+++ src/comp/CompNaverMap.jsx
@@ -15,7 +15,6 @@ import nodeSel from '../../public/images/node_sel.png';
 const navermaps = window.naver.maps
 
 const CompNaverMap = (props) => {
-    console.log("11111");
     const { setMapMinMax, mapCenterLonLat, zoomSize, markerAddOpt, markerEvtOpt, polylineAddOpt, polylineEvtOpt, ...others} = props;
 
     //네이버지도
